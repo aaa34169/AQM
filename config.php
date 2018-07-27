8341d39d94b251febd1479ec82238749d0a77fb4
ISDADS\HLS501 <fabien.leboeuf@gmail.com>
2018-07-27 11:03:30 +0100

    config file modified

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=aqm_chru','root','PASSWORD',array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
-$local_path = '~/www/AQM/';
+$db = new PDO('mysql:host=localhost;dbname=aqm_chru','root','',array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+$local_path = 'C:/wamp64/www/CGA_DataBase/';
 ?>
